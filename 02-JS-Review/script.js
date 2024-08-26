5bf496dd7ab9ab27291189f11edcd88a4196e196
Joelson Junior <joelson.ejr@gmail.com>
2024-08-26 20:37:12 -0300

    Async/Await - end of review

--- 02-JS-Review/script.js
+++ 02-JS-Review/script.js
@@ -347,9 +347,18 @@ booksAfterUpdate;
 
 /////////////////////////////////////////////////////////////////////
 //Asynchronous JavaScript: Promises
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((resp) => resp.json())
+//   .then((data) => console.log(data));
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((resp) => resp.json())
-  .then((data) => console.log(data));
+// console.log(`Print after request`);
 
-console.log(`Print after request`);
+/////////////////////////////////////////////////////////////////////
+//Asynchronous JavaScript: Async/ Await
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+getTodos();
