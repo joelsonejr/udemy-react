777ac48590c924cdbe02b119ebd744250d1d24ad
Joelson Junior <joelson.ejr@gmail.com>
2024-09-23 12:26:00 -0300

    Lesson 33: removing unnecessary info fom index.js

--- 05-pizza-menu/src/index.js
+++ 05-pizza-menu/src/index.js
@@ -5,13 +5,9 @@ const App = () => {
   return <h1>Hello React!</h1>;
 };
 
-//React version 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-//Older versions of React
-// React.render(<App />);
