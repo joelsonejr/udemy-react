8153b3d43c1eae146927e59d8f17e8e37bbd8712
Joelson Junior <joelson.ejr@gmail.com>
2025-11-04 20:46:55 -0300

    finished popcornapp phase 01

--- codingLessons/07-usepopcorn/src/index.js
+++ codingLessons/07-usepopcorn/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./components/StarRating";
+// import "./index.css";
+// import App from "./App";
+
+const messages = ["Terrible", "Bad", "Okay", "Goog", "Amazing"];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={"5"} messages={messages} />
+    <StarRating maxRating={10} size={30} color="green" defaultRating={3} />
+    <StarRating color="red" size={60} onSetRating />
   </React.StrictMode>
 );
